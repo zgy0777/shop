d4df2911e16c809ced51f8293304281d3df7e116
zgy0777 <soldier76@qq.com>
2018-12-05 14:28:23 +0000

    完成收货地址curd

--- app/Http/Controllers/UserAddressController.php
+++ app/Http/Controllers/UserAddressController.php
@@ -23,6 +23,7 @@ class UserAddressController extends Controller
         return view('user_addresses.create_and_edit',['address'=> new UserAddress()]);
     }
 
+    //提交行为
     public function store(UserAddressRequest $request)
     {
         //为该用户添加收货地址，只允许以下字段注入
@@ -37,6 +38,44 @@ class UserAddressController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+
+    }
+
+    //更新视图,查出所有数据
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own',$user_address);
+
+        return view('user_addresses.create_and_edit',['address' => $user_address]);
+    }
+
+    //更新行为
+    public function update(UserAddress $user_address,UserAddressRequest $request)
+    {
+        $this->authorize('own',$user_address);
+
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect(route('user_addresses.index'));
+
+    }
+
+    //删除行为
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own',$user_address);
+
+        $user_address->delete();
+
+        return [];
 
     }
 
