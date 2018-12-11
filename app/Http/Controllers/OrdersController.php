6df7b7082b4de75680b04be72f8bb838ef05cb3b
zgy0777 <soldier76@qq.com>
2018-12-11 03:35:06 +0000

    用户界面 -订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -73,4 +73,19 @@ class OrdersController extends Controller
 
         return $order;
     }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            //使用with预加载，避免N+1问题
+              ->with(['items.product','items.productSku'])
+              ->where('user_id',$request->user()->id)
+              ->orderBy('created_at','desc')
+              ->paginate();
+
+        return view('orders.index',['orders'=>$orders]);
+
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -41,12 +41,13 @@ Route::group(['middleware' => 'auth'],function(){
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
-
+        //购物车
         Route::post('cart','CartController@add')->name('cart.add');
         Route::get('cart','CartController@index')->name('cart.index');
         Route::delete('/cart/{sku}','CartController@remove')->name('cart.remove');
-
+        //个人用户中心
         Route::post('orders', 'OrdersController@store')->name('orders.store');
+        Route::get('orders','OrdersController@index')->name('orders.index');
 
     });
 });
